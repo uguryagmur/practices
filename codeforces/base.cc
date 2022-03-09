24e92894eac5fa5313e38e51aeed326fac2bbb7f
uguryagmur <cuy.official@gmail.com>
2022-03-09 21:51:58 +0000

    base file for comtests is added

---
+++ codeforces/base.cc
@@ -0,0 +1,35 @@
+// Competitive programming base	C++ file
+//
+// Author: C. Ugur Yagmur
+//
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+#define hmap unordered_map
+#define hset unordered_set
+
+
+using namespace std;
+
+typedef double d;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<d> vd;
+typedef vector<string> vs;
+
+
+void solve(){
+}
+
+
+int main(){
+	return 0;
+}
